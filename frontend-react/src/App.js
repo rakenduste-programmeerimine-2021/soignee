7d6b4c56f0fb951dee79edd28ea18fc90f755f88
wemitwqw <55079623+wemitwqw@users.noreply.github.com>
2021-11-19 00:30:21 +0200

    standup1

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,84 +1,26 @@
-import "antd/dist/antd.css";
 import "./index.css";
 
-import { Layout, Menu } from 'antd';
-import { HomeTwoTone, ThunderboltTwoTone } from '@ant-design/icons';
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Route, Link, Routes } from "react-router-dom";
 
-import Posts from './pages/Posts';
-import AddPost from './pages/AddPost';
-import EditPost from './pages/EditPost';
 import Home from './pages/Home';
-
-const { Header, Content, Footer, Sider } = Layout;
-const SubMenu = Menu.SubMenu;
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
 
-
-
-  // let active=window.location.href.split('/');
-  // active=active[active.length - 1];
-  let activePage = window.location.pathname;
-  // const [activePage, setActivePage] = useState(active);
-
-  // const toggle = () => setCollapsed(!collapsed);
-  // const [openKeys, setOpenKeys] = React.useState(['postsMenu']);
-  const openKeys = ['postsMenu'];
-
-  const [collapsed, setCollapsed] = useState(false);
-
-  const onCollapse = (collapsed) => setCollapsed(collapsed);
-
   return (
     <Router>
-      <Layout style={{ minHeight: '100vh' }}>
-        <Sider
-            collapsible
-            collapsed={collapsed}
-            onCollapse={onCollapse}>
-            <div className="logo" />
-            <Menu theme="dark" defaultSelectedKeys={[activePage]} mode="inline" defaultOpenKeys={openKeys}>
-                <Menu.Item key="/">
-                    <HomeTwoTone/>
-                    <span>Home</span>
-                    <Link to="/" />
-                </Menu.Item>
-                <SubMenu key="postsMenu" icon={<ThunderboltTwoTone />} title="Posts">
-                  <Menu.Item key="/posts">
-                    <span>Show Posts</span>
-                    <Link to="/posts" />
-                  </Menu.Item>
-                  <Menu.Item key="/addpost">
-                    <span>Add Post</span>
-                    <Link to="/addpost" />
-                  </Menu.Item>
-                </SubMenu>
-            </Menu>
-        </Sider>
-        <Layout>  
-            <Header style={{ background: '#fff', padding: 0, paddingLeft: 16 }}>
-                {/* <HomeTwoTone 
-                    className="trigger"
-                    type={collapsed ? 'menu-unfold' : 'menu-fold'}
-                    style={{ cursor: 'pointer' }}
-                    onClick={toggle}
-                /> */}
-            </Header>
-            <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280 }}>
-                <Routes>
-                    <Route exact path="/" element={<Home/>} />
-                    <Route path="/posts" element={<Posts/>} />
-                    <Route path="/addpost" element={<AddPost/>} />
-                    <Route path="/editpost/:post_id" element={<EditPost/>} />
-                </Routes>
-            </Content>
-            <Footer style={{ textAlign: 'center' }}>
-                Projekt on loodud aine IFI6229.DT raames.
-            </Footer>
-        </Layout>
-      </Layout>
+      <Header/>
+        <Routes>
+            <Route exact path="/" element={<Home/>} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/signup" element={<Signup/>} />
+        </Routes>
+      {/* <Footer/> */}
     </Router>
   );
 }
--- frontend-react/src/pages/Home.js
+++ frontend-react/src/pages/Home.js
@@ -1,9 +1,95 @@
-function Home() {
-    return ( 
-    <div>
-        :^)
-    </div> 
-    );
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const theme = createTheme();
+
+function Album() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+        {/* Hero unit */}
+        <Box
+          sx={{
+            bgcolor: 'background.paper',
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="sm">
+            {/* <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="text.primary"
+              gutterBottom
+            >
+              Album layout
+            </Typography> */}
+            <Typography variant="h5" align="center" color="text.secondary" paragraph>
+                soignée<br/>
+                ADJECTIVE<br/>
+                dressed and groomed elegantly and with great care
+            </Typography>
+            <Stack
+              sx={{ pt: 4 }}
+              direction="row"
+              spacing={2}
+              justifyContent="center"
+            >
+              {/* <Button variant="contained">Main call to action</Button>
+              <Button variant="outlined">Secondary action</Button> */}
+            </Stack>
+          </Container>
+        </Box>
+        <Container sx={{ py: 2 }} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                >
+                  <CardMedia
+                    component="img"
+                    sx={{
+                      // 16:9
+                      pt: '56.25%',
+                    }}
+                    image="https://source.unsplash.com/random"
+                    alt="random"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Trackpants
+                    </Typography>
+                    <Typography>
+                      Short description
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">View</Button>
+                    <Button size="small">Edit</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+    </ThemeProvider>
+  );
 }
 
-export default Home;
+export default Album;
