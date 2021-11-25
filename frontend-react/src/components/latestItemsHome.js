3a02920ad7219710a720b964da24f8b3c4d6506e
wemitwqw <55079623+wemitwqw@users.noreply.github.com>
2021-11-25 17:24:54 +0200

    upd

---
+++ frontend-react/src/components/latestItemsHome.js
@@ -0,0 +1,58 @@
+import React, {useEffect} from 'react';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { CardActionArea } from '@mui/material';
+import TextField from '@mui/material/TextField';
+import { Link } from 'react-router-dom';
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function LatestItemsHome() {
+    return (
+        <Grid container spacing={4}>
+            {cards.map((card) => (
+            <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card
+                sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                >
+                <CardActionArea component={Link} to={{ pathname: '/listing/' }} >
+                    <CardMedia
+                    component="img"
+                    sx={{
+                        // 16:9
+                        pt: '0%',
+                    }}
+                    image="https://source.unsplash.com/random"
+                    alt="random"
+                    />
+                    <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        BRANDNAME
+                    </Typography>
+                    <Typography>
+                        Windbreaker
+                    </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button size="small">Edit</Button>
+                    <Button size="small">Delete</Button>
+                </CardActions>
+                </Card>
+            </Grid>
+            ))}
+        </Grid>
+    )
+}
+
+export default LatestItemsHome
--- frontend-react/src/pages/Home.js
+++ frontend-react/src/pages/Home.js
@@ -1,24 +1,13 @@
 import React, {useEffect} from 'react';
-import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { CardActionArea } from '@mui/material';
 import TextField from '@mui/material/TextField';
-import { Link } from 'react-router-dom';
 
 import AuthService from "../Auth/AuthService";
-import LatestItemsHome from '../components/latestItemsHome';
-
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+import LatestItemsHome from '../components/LatestItemsHome';
 
 const theme = createTheme();
 
