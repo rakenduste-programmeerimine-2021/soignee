80cd5cb4ada515cd707e4e8206784e0d525bf382
wemitwqw <55079623+wemitwqw@users.noreply.github.com>
2021-11-25 16:59:40 +0200

    Alek

--- frontend-react/src/pages/Profile.js
+++ frontend-react/src/pages/Profile.js
@@ -20,94 +20,11 @@ import AuthService from "../Auth/AuthService";
 
 const theme = createTheme();
 
-function Profile({auth, setAuth}) {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
-  const [loginok, setLoginok] = useState(false);
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-    const authData = await AuthService.login(email,password)
-    setLoginok(true);
-    setAuth(authData);
-  }
-
-  if (loginok || auth) {
-    return (
-      <Navigate to="/" />
-    )
-  }
-
+function Profile() {
   return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          
-          <Typography component="h1" variant="h5">Sign in</Typography>
-
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              onChange={e => setEmail(e.target.value)}
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              onChange={e => setPassword(e.target.value)}
-            />
-            {/* <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            /> */}
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              {/* <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid> */}
-              <Grid item>
-                <Link href="/signup" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-      </Container>
-    </ThemeProvider>
-  );
+      <>
+      </>
+    );
             }
 
 
