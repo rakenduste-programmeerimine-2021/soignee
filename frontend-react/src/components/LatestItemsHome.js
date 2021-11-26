65f8b120e45d75497a4a228368fc2eb32013cd15
Vladislav Priske <55079623+wemitwqw@users.noreply.github.com>
2021-11-26 12:05:23 +0200

    capitalized file

---
+++ frontend-react/src/components/LatestItemsHome.js
@@ -0,0 +1,90 @@
+import React, {useEffect, useState} from 'react';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { CardActionArea } from '@mui/material';
+import TextField from '@mui/material/TextField';
+import { Link } from 'react-router-dom';
+
+const styles = {
+    card: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 0,
+      paddingTop: '56.25%', // 16:9
+    },
+  };
+ 
+function LatestItemsHome() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [loadedItems, setLoadedItems] = useState([]);
+
+    useEffect(() => {
+    fetch('http://localhost:8081/api/items/latest').then(res => { 
+    return res.json(); 
+    }).then(data => {
+    console.log(data);
+    setIsLoading(false);
+    setLoadedItems(data);
+    });
+    },[])
+    
+    if (isLoading) {
+        return (<div>Laeb...</div>);
+    }
+    
+    return (        
+        <Grid container spacing={4}>
+            {loadedItems.map(({brandName, model, _id, price, photo}) => (
+            <Grid item key={_id} xs={12} sm={6} md={4}>
+                <Card
+                sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                >
+                <CardActionArea component={Link} to={{ pathname: '/item/' + _id }} >
+                    <CardMedia
+                    // style = {{ height: 400, paddingTop: '0%'}}
+                    component="img"
+                    sx={{
+                        // 16:9
+                        pt: '0%',
+                    }}
+                    height="400"
+                    image={"/uploaded/" + photo}
+                    alt="image"
+                    />
+                    <CardContent sx={{ flexGrow: 1 }}>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            {model}
+                        </Typography>
+                        <Grid>
+                            <Typography>
+                                {brandName}
+                            </Typography>
+                            <Typography>
+                                {price+'€'}
+                            </Typography>
+                        </Grid>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button size="small" component={Link} to={{ pathname: '/item/edit/' + _id }}>Edit</Button>
+                    <Button size="small" component={Link} to={{ pathname: '/item/delete/' + _id }}>Delete</Button>
+                </CardActions>
+                </Card>
+            </Grid>
+            ))}
+        </Grid>
+    )
+}
+
+export default LatestItemsHome
---
+++ frontend-react/src/components/ProfileInfo.js
@@ -0,0 +1,88 @@
+import React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useState, useEffect } from "react";
+import { Navigate } from "react-router";
+import { borders } from "@mui/system";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+
+import AuthService from "../Auth/AuthService";
+
+const theme = createTheme();
+
+function ProfileInfo() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Container sx={{ pt: 2, textAlign: "center" }} maxWidth="xs">
+        <Typography component="h1" variant="h5">
+          Profile
+        </Typography>
+      </Container>
+      <Container
+        sx={{
+          py: 2,
+          display: "flex",
+          flexWrap: "wrap",
+          alignItems: "center",
+          justifyContent: "center",
+          textAlign: "center",
+        }}
+        maxWidth="md"
+      >
+        <Box
+          sx={{
+            minHeight: "250px",
+            width: "50",
+            flex: "auto",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            overflow: "hidden",
+
+
+
+          }}
+        >
+          <AccountCircleIcon sx={{
+              color: "#000000",
+              fontSize: "200px",
+          }} />
+        </Box>
+        <Box
+            sx={{
+                display: "flex",
+                flex: "1 0 auto",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                textAlign: "center"
+            }}
+        >
+            <Typography sx={{ minHeight: "60px", textAlign: "left"}} component="h1" variant="h5">
+                First Name: 
+            </Typography>
+            <Typography sx={{ minHeight: "60px", textAlign: "left"}} component="h1" variant="h5">
+                Last Name: 
+            </Typography>
+            <Typography sx={{ minHeight: "60px", textAlign: "left"}} component="h1" variant="h5">
+                Email: 
+            </Typography>
+
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default ProfileInfo;
