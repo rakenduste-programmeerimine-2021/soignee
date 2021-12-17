ac8ee64a7da2a0cbdba037669b72a2c5c1499fb4
Alek-Jaan Tsern <winlanto@tlu.ee>
2021-12-17 05:39:30 +0200

    Single item view layout done

--- frontend-react/src/pages/ViewListing.js
+++ frontend-react/src/pages/ViewListing.js
@@ -3,10 +3,10 @@ import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Typography from "@mui/material/Typography";
-import { Stack } from '@mui/material';
 import { useState, useEffect } from 'react';
 import { Navigate, useParams } from 'react-router';
 import CardMedia from '@mui/material/CardMedia';
+import { Link } from 'react-router-dom';
 
 
 const theme = createTheme();
@@ -41,60 +41,78 @@ function ViewListing({loginok}) {
 
   return (
     <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <Box
-            sx={{
-              marginTop: 8,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              overflow: 'hidden',
-              textAlign: "center",
-              justifyContent: 'center'
-            }}
+      <Container component="main" sx={{
+          py: 2,
+          display: "flex",
+          flexWrap: "wrap",
+          alignItems: "center",
+          justifyContent: "center",
+          textAlign: "center",
+        }}
+        maxWidth="md">
+          <Box
+          sx={{
+            height: "350px",
+            width: "50%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            overflow: "hidden",
+            flex: "1 1 auto" 
+
+
+
+          }}
         >
-          <Stack spacing={2}>
-            <Box>
-              <Typography sx={{ minHeight: "60px", fontWeight: "700"}} component="h2" variant="h2">
-                  {loadedItem[0]["brandName"]} 
-              </Typography>
-            </Box>
-            <CardMedia 
+          <CardMedia 
               component="img"
               sx={{
                 pt: '0%',
+                display: "flex",
+                height: "300px",
+                width: "300px"
+
               }}
-              maxHeight="200"
+
               margin="auto"
               image={`http://localhost:8081/api/image/${loadedItem[0]["photo"]}`}        
             />
-            <Box>
+          </Box>
+          <Box
+            sx={{
+                display: "flex",
+                flex: "1 0 auto",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                textAlign: "center"
+            }}
+        >
+              <Typography sx={{ minHeight: "60px", fontWeight: "700"}} component="h2" variant="h2">
+                  {loadedItem[0]["brandName"]} 
+              </Typography>
               <Typography sx={{ minHeight: "60px", fontWeight: "700" }} component="h5" variant="h5">
                   Model: {loadedItem[0]["model"]}
               </Typography>
-            </Box>
-            <Box>
               <Typography sx={{ minHeight: "60px", fontWeight: "700"}} component="h5" variant="h5">
                   Price: {loadedItem[0]["price"]}€
               </Typography>
-            </Box>
-            <Box>
               <Typography sx={{ minHeight: "60px", fontWeight: "700"}} component="h5" variant="h5">
                   Description 
                   <Typography sx={{ fontSize: "1.5rem", p: "20px", fontWeight: "300"}} component="p" variant="p">
                   {loadedItem[0]["description"]}
                   </Typography>
               </Typography>
-            </Box>
-            <Typography sx={{ minHeight: "60px", fontWeight: "700"}} component="h5" variant="h5">
+              <Typography sx={{ minHeight: "60px", fontWeight: "700"}} component="h5" variant="h5">
                   Posted at: 
                   <Typography sx={{ fontSize: "1.5rem", p: "20px", fontWeight: "300"}} component="p" variant="p">
                   {loadedItem[0]["createdAt"]}
                   </Typography>
               </Typography>
-          </Stack>
-
-        </Box>
+              <Typography sx={{ fontSize: "1.5rem", p: "20px", fontWeight: "300"}} component="p" variant="p">
+                  User: <Link to={`/profile/${loadedItem[0]["user"]}`}>{loadedItem[0]["user"]}</Link>
+              </Typography> 
+            </Box>
       </Container>
     </ThemeProvider>
   );
